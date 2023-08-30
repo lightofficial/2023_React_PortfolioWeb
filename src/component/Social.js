40b550eace0a3111486cd11453bbe2a2c1a57f4b
THANAT PROMPIRIYA <78747621+lightofficial@users.noreply.github.com>
2023-08-30 05:11:34 +0000

    Update Social.js

--- src/component/Social.js
+++ src/component/Social.js
@@ -4,9 +4,9 @@ class Social extends Component {
     render()    {
         return(
             <div className="social">
-                <a href="https://github.com/lightofficial" target="_blank" rel="noopender noreferrer"><i class="fab fa-github"></i></a>
-                <a href="https://www.instagram.com/g.guily/" target="_blank" rel="noopender noreferrer"><i class="fab fa-instagram"></i></a>
-                <a href="https://web.facebook.com/GuilKung/" target="_blank" rel="noopender noreferrer"><i class="fab fa-facebook"></i></a>
+                <a href="https://github.com/lightofficial" target="_blank" rel="noopender noreferrer"><i className="fab fa-github"></i></a>
+                <a href="https://www.instagram.com/g.guily/" target="_blank" rel="noopender noreferrer"><i className="fab fa-instagram"></i></a>
+                <a href="https://web.facebook.com/GuilKung/" target="_blank" rel="noopender noreferrer"><i className="fab fa-facebook"></i></a>
             </div>
         )
     }
